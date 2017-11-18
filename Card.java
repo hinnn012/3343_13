dc558ef69716e30f051cae2e06fea11af2b37148
Hin <Hin@Hin-PC>
2017-11-18 16:01:58 +0800

    Added Card class with constructor and get method.

--- Card.java
+++ Card.java
@@ -1,1 +1,29 @@
+package Big_Two;
+
+public class Card {
+
+	private char face;	//e.g. ace to king
+	private char suit;	//e.g. diamond to spade
+	
+	public Card(char cFace, char cSuit) {
+		
+		cFace = face;
+		cSuit = suit;
+		
+		
+	}
+	
+	public char getFace() {
+		
+		return face;
+		
+	}
+	
+	public char getSuit() {
+		
+		return suit;
+		
+	}
+	
+}
 

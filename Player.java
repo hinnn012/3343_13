c3fe8e1219f21a23c399e37c904e70b4fc1e988e
hin <31984469+hinnn012@users.noreply.github.com>
2017-11-23 15:03:59 +0800

    Add files via upload
    added player class with methods (not yet finished)

---
+++ Player.java
@@ -0,0 +1,48 @@
+package Big_two;
+import java.util.ArrayList;
+
+public class Player {
+	
+	private ArrayList<Card> cardList ;
+	private String name;
+	private String ValidAction = null;
+	
+	public Player(String aName) {
+		
+		aName = name;
+		cardList = new ArrayList<>();
+		
+	}
+	
+	public void addHands(Card card) {
+		
+		cardList.add(card);
+		
+		
+	}
+	public String showHands() {
+		
+		for(Card c : cardList) {
+			
+			
+			
+		}
+		
+		return null;
+	}
+	
+	public boolean Pass() {
+		
+		
+		return false;
+	}
+	
+	public boolean Play(int x[]) {
+		
+		
+		
+		return false;
+	}
+	
+	
+}

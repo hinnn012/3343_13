8dd6fec795c5f538f707845d9abb76349f888994
hin <31984469+hinnn012@users.noreply.github.com>
2017-11-25 13:10:52 +0800

    main modification

--- BigTwo/Main.java
+++ BigTwo/Main.java
@@ -1,6 +1,8 @@
 package BigTwo;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+
 import BigTwo.Card;
 import BigTwo.Player;
 import BigTwo.Poker;
@@ -13,9 +15,11 @@ public class Main {
 		String history = null;
 		String input = null;
 		boolean end = false; 
-		boolean ture = false;
+		boolean true_input = false;
+		int player_index = 0;
 		ArrayList<Player> player = new ArrayList<Player>();
-		
+		ArrayList<String> history = new ArrayList<String>();
+		String tmp_history;
 		System.out.println("Initializing...");
 		
 		
@@ -42,8 +46,98 @@ public class Main {
 		try {
 			while(end == false){
 				
-				break; // break infinite loop , will be removed in final
+				true_input = false;
+				System.out.println("Player " + ( player_index + 1 ) + "turn");
+				System.out.println("=============================================================================================");
+				Player this_turn = player.get(player_index);
+				System.out.println("History: ");
 				
+				for(String s : history) {
+					
+					System.out.println(s);
+					
+				}
+				
+				System.out.printf("Your Hand: {	 %s } /n", this_turn.showHands());
+				
+				while(!true_input) {
+					
+					System.out.print("Please enter your action (history/play/pass): ");
+					Scanner c = new Scanner(System.in);
+					input = c.next();
+					
+					if(input == "history") {
+							
+							System.out.print("\033[H\033[2J");  
+							System.out.flush(); 
+							for(String s : history) {
+							
+							System.out.println(s);
+							
+						}
+						
+					}
+					
+					else if(input == "play") {
+						
+						String card_to_play = "";
+						card_to_play = c.next() + " ";
+						String[] card_index =  card_to_play.split(" ");
+						int [] card_inedx_int = new int[card_index.length];
+						
+
+						for(int i = 0; i < card_index.length; i++) {
+							
+							card_inedx_int[i] = Integer.parseInt(card_index[i]);
+							
+						}
+						
+						if(this_turn.Play(card_inedx_int)) {
+							
+							
+							System.out.println("You have played " + this_turn.returnlastAction());
+							tmp_history = "Player " + (player_index + 1) + "played" + this_turn.returnlastAction() + "."; 
+							history.add(tmp_history);
+							true_input = true;
+						}
+						
+						else System.out.print("Your Input Pattern is not correct!");
+						
+					}
+					
+					else if(input == "pass") {
+						
+						
+						if(this_turn.Pass()) {
+							tmp_history = "Player " + (player_index + 1) + "passed."; 
+							history.add(tmp_history);
+							true_input = true;
+						}
+						
+					}
+					
+					else {
+						
+						
+						
+					}
+					c.close();
+				}
+				
+				
+				
+				player_index++;
+				
+				if(player_index == 4) {
+					player_index = 0;
+				}
+				
+				
+				//break; // break infinite loop , will be removed in final
+				
+				//clear console
+				System.out.print("\033[H\033[2J"); 
+			    System.out.flush(); 
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block

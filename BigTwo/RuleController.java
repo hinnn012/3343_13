6bbf01c519e7a09d250a45b5fe41a5ef4e2246e4
arcal <arcal@192.168.3.58>
2017-11-25 12:02:01 +0800

    update for case 10JQKA

--- BigTwo/RuleController.java
+++ BigTwo/RuleController.java
@@ -67,8 +67,15 @@ public class RuleController {
 	}
 			
 	public boolean Straight(ArrayList<Card> arraylist){
+		if(faceToInt(arraylist.get(0).getFace())!=10){
 		for(int i = 0 ; i < arraylist.size() ; i++){
-			if(faceToInt(arraylist.get(i).getFace())!=faceToInt(arraylist.get(i+1).getFace())+1){
+			if(faceToInt(arraylist.get(i).getFace())!=faceToInt(arraylist.get(i+1).getFace())-1){
+				valid = false;
+				break;
+			}else valid = true;
+		}
+		}else for(int i = 0 ; i < arraylist.size()-1 ; i++){
+			if(faceToInt(arraylist.get(i).getFace())!=faceToInt(arraylist.get(i+1).getFace())-1&&faceToInt(arraylist.get(arraylist.size()-1).getFace())!=1){
 				valid = false;
 				break;
 			}else valid = true;

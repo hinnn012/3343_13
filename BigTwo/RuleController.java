2a745edbe92052550f3a2be341d180e47d961f41
arcal <arcal@192.168.3.58>
2017-11-25 11:30:07 +0800

    update striaght

--- BigTwo/RuleController.java
+++ BigTwo/RuleController.java
@@ -17,7 +17,28 @@ public class RuleController {
 			
 	private Integer lastRank;
 	
-	Card card;
+	public Integer faceToInt(String face){
+		Integer intFace = null;
+		switch(face){
+		case "A" :intFace= 1;
+		case "2" :intFace= 2;
+		case "3" :intFace= 3;
+		case "4" :intFace= 4;
+		case "5" :intFace= 5;
+		case "6" :intFace= 6;
+		case "7" :intFace= 7;
+		case "8" :intFace= 8;
+		case "9" :intFace= 9;
+		case "10":intFace= 10;
+		case "J" :intFace= 11;
+		case "Q" :intFace= 12;
+		case "K" :intFace= 13;
+		}
+		return intFace;
+		
+	}
+	
+	boolean valid = false;
 	
 	public boolean valid(){
 		return false;
@@ -27,27 +48,44 @@ public class RuleController {
 	
 	public boolean Pair(ArrayList<Card> arraylist){
 		for (int i=0;i < arraylist.size();i++){
-			
+		if (arraylist.get(i).getFace()==arraylist.get(i+1).getFace())
+			valid = true;
 		}
-		return false;
+		return valid;
 
 		
 	}
 			
 	public boolean ThreeOfKind(ArrayList<Card> arraylist){
-		return false;
+		for (int i=0;i < arraylist.size();i++){
+			if (arraylist.get(i).getFace()==arraylist.get(i+1).getFace()&&arraylist.get(i+2).getFace()==arraylist.get(i+1).getFace())
+				valid = true;
+			}
+		return valid;
 
 			
 	}
 			
 	public boolean Straight(ArrayList<Card> arraylist){
-		return false;
+		for(int i = 0 ; i < arraylist.size() ; i++){
+			if(faceToInt(arraylist.get(i).getFace())!=faceToInt(arraylist.get(i+1).getFace())+1){
+				valid = false;
+				break;
+			}else valid = true;
+		}
+		return valid;
 				
 			
 	}
 			
 	public boolean Flush(ArrayList<Card> arraylist){
-		return false;
+		for(int i = 0 ; i < arraylist.size() ; i++){
+			if(arraylist.get(i).getSuit()!=arraylist.get(i+1).getSuit()){
+				valid = false;
+				break;
+			}else valid = true;
+		}
+		return valid;
 				
 			
 	}
@@ -55,6 +93,6 @@ public class RuleController {
 
 	public static RuleController getInstance() {
 		// TODO Auto-generated method stub
-		return null;
+		return instance;
 	}
 }

cc149fa2c842c91a63a42645ebbffcb088f23f27
hin <31984469+hinnn012@users.noreply.github.com>
2017-11-25 13:13:19 +0800

    changed history from string to arraylist

--- BigTwo/Main.java
+++ BigTwo/Main.java
@@ -12,7 +12,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		String history = null;
+		//String history = null;
 		String input = null;
 		boolean end = false; 
 		boolean true_input = false;

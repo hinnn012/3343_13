871988f209002c18e25d6ba48b4c15077e6feeca
sadase <jklifos@gmail.com>
2017-11-25 23:59:20 +0800

    Test for Face

---
+++ Test/testFace.java
@@ -0,0 +1,24 @@
+package Test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import BigTwo.Face;
+import BigTwo.Card;
+
+public class testFace {
+
+	@Test
+	public void testFace_1() throws Exception {
+	
+		Face face = null;
+		assertEquals("1",face.A.getValue());
+		
+	}
+	
+	@Test
+	public void testFace_2() throws Exception {
+		
+		Face face = null;
+		assertEquals("9",face.NINE.getValue());
+	}
+}

44e3f48c054e88cd7393eec4867968ffb5078583
hin <31984469+hinnn012@users.noreply.github.com>
2017-11-26 22:42:09 +0800

    added getIntFace() for value and compareTo() for sorting arraylist

--- BigTwo/Card.java
+++ BigTwo/Card.java
@@ -1,6 +1,6 @@
 package BigTwo;
 
-public class Card {
+public class Card implements Comparable<Card>{
 
 	private String face;	//e.g. ace to king
 	private String suit;	//e.g. diamond to spade
@@ -24,5 +24,36 @@ public class Card {
 		
 	}
 	
+	public int getIntFace() {
+		
+		Integer intFace = 0;
+		switch(face){
+		case "A" :intFace= 12;break;
+		case "2" :intFace= 13;break;
+		case "3" :intFace= 1;break;
+		case "4" :intFace= 2;break;
+		case "5" :intFace= 3;break;
+		case "6" :intFace= 4;break;
+		case "7" :intFace= 5;break;
+		case "8" :intFace= 6;break;
+		case "9" :intFace= 7;break;
+		case "10":intFace= 8;break;
+		case "J" :intFace= 9;break;
+		case "Q" :intFace= 10;break;
+		case "K" :intFace= 11;break;
+		}
+		return intFace;
+		
+		
+		
+	}
+
+	@Override
+	public int compareTo(Card next) {
+		if(this.getIntFace() > next.getIntFace()) return 1;
+		else if(this.getIntFace() < next.getIntFace()) return 0;
+		// TODO Auto-generated method stub
+		return -1;
+	}
 }
 

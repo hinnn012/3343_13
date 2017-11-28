93a2af55964b0c96e3966d3809802236df61cc61
Cheung Yu Yiu <32504402+yuycheung6@users.noreply.github.com>
2017-11-28 17:01:23 +0800

    Structure modification
    re-modify code to let straight work as intend and fix the problem of print message with straight cases, add new exception handle

--- BigTwo/RuleController.java
+++ BigTwo/RuleController.java
@@ -7,10 +7,10 @@ import java.util.Collections;
 
 import BigTwo.Card;
 import Exception.CannotPassYourOwnLoopException;
-import Exception.InputCannotBeNullException;
 import Exception.InputNotValidException;
 import Exception.InvalidPatternException;
 import Exception.InvalidRankException;
+import Exception.PatternNotRecognizeException;
 
 
 public class RuleController {
@@ -46,13 +46,20 @@ public class RuleController {
 	
 	public String getLastPlayMsg() {
 		
-		return lastValidPlayer + " " + lastPattern + " a " + lastPattern + ".";
+		return lastValidPlayer + " " + lastCards + " a " + lastPattern + ".";
 		
 	}
 	
-	public void setLastCards(ArrayList<Card> card) {
-		
-		for(Card c : card) {
+	public void setLastCards(ArrayList<Card> card,boolean flag) {
+		if (flag)
+		{
+			ArrayList<Card> mod = new ArrayList<Card>();
+			mod = ascenCard(card);
+			for (Card c:mod) {
+				this.lastCards += c.getSuit() + c.getFace() + " ";
+			}
+		}
+		else for(Card c : card) {
 			
 			this.lastCards += c.getSuit() + c.getFace() + " ";
 			
@@ -63,6 +70,24 @@ public class RuleController {
 	public void setLastWeight(int weight)
 	{
 		this.lastWeight = weight;
+	}
+	
+	public ArrayList<Card> ascenCard(ArrayList<Card> cardArray){
+		ArrayList<Card> temp = new ArrayList<>();
+		for(Card c:cardArray) {
+			if (c.getFace().equals("A")||c.getFace().equals("2"))
+			{
+				temp.add(c);
+				cardArray.remove(c);
+			}
+			
+		}
+		for(Card c:cardArray) {			
+				temp.add(c);
+				cardArray.remove(c);			
+		}
+		return temp;
+		
 	}
 
 
@@ -93,42 +118,96 @@ public class RuleController {
 	
 	//boolean valid = false;
 	
-	public boolean valid(ArrayList<Card> card_to_be_played, String name) throws InputNotValidException, InvalidPatternException, InvalidRankException, CannotPassYourOwnLoopException{
+	public boolean passCheck(String name)throws CannotPassYourOwnLoopException{
 		
+		if (this.getLastValidPlayer().equals(name))throw new CannotPassYourOwnLoopException(name);
+		else return true;
+		
+	}
+	public boolean straightSmall(String pattern,ArrayList<Card> array) {
+		if  ((pattern.equals("Straight")||pattern.equals("Straight Flush"))&&((array.get(3).getFace().equals("A")&&array.get(4).getFace().equals("2"))||!(array.get(3).getFace().equals("A")&&array.get(4).getFace().equals("2"))))return true;
+		else return false;
+	}
+	public boolean valid(ArrayList<Card> card_to_be_played, String name) throws InputNotValidException, InvalidPatternException, InvalidRankException, PatternNotRecognizeException{
+		
+	Collections.sort(card_to_be_played);
 	String pattern = checkPattern(card_to_be_played);
-	if (lastValidPlayer.equals("")&&lastPattern.equals("")&&lastRank==0&&lastWeight==0)
+	if (lastValidPlayer.equals("")&&lastPattern.equals("")&&lastRank==0&&lastWeight==0&&!pattern.equals("Invalid"))
 	{
-		setLastPattern(pattern);
-		int rank = calRank(card_to_be_played);
-		int weight = calWeight(pattern);
-		setLastRank(rank);
-		setLastWeight(weight);
-		setLastCards(card_to_be_played);
-		return true;	
+		
+		if (straightSmall(pattern,card_to_be_played)) 
+		{
+			setLastPattern(pattern);
+			int rank = calRank(card_to_be_played,true);
+			int weight = calWeight(pattern);
+			setLastRank(rank);
+			setLastWeight(weight);
+			setLastCards(card_to_be_played,true);
+			return true;
+		}
+		else
+		{
+			setLastPattern(pattern);
+			int rank = calRank(card_to_be_played,false);
+			int weight = calWeight(pattern);
+			setLastRank(rank);
+			setLastWeight(weight);
+			setLastCards(card_to_be_played,false);
+			return true;
+		}
 	}
 	else if(!this.lastValidPlayer.equals("") && !pattern.equals(this.getLastPattern())){
 		throw new InvalidPatternException(pattern);
-	}else if(!this.lastValidPlayer.equals("") &&pattern.equals(this.lastPattern)){
-		int rank = calRank(card_to_be_played);
+	}else if(!this.lastValidPlayer.equals(name)&&!this.lastValidPlayer.equals("") &&pattern.equals(this.lastPattern)&&!pattern.equals("Invalid")){
+		if (straightSmall(pattern,card_to_be_played)) {
+		int rank = calRank(card_to_be_played,true);
 		int weight = calWeight(pattern);
 		if (rank>lastRank && weight>lastWeight)
 		{
 		setLastRank(rank);
 		setLastWeight(weight);
-		setLastCards(card_to_be_played);
+		setLastCards(card_to_be_played,true);
 		return true;	
 		}
+		
 		else throw new InvalidRankException(rank);
-	}else if(this.lastValidPlayer.equals(name)&&!pattern.equals("null")){
+		}
+		else {
+		int rank = calRank(card_to_be_played,false);
+		int weight = calWeight(pattern);
+		if (rank>lastRank && weight>lastWeight)
+		{
+		setLastRank(rank);
+		setLastWeight(weight);
+		setLastCards(card_to_be_played,false);
+		return true;	
+		}
+		
+		else throw new InvalidRankException(rank);
+		}
+	}else if(this.lastValidPlayer.equals(name)&&!pattern.equals("null")&&!pattern.equals("Invalid")){
+		if (straightSmall(pattern,card_to_be_played)) {
 		this.setLastPattern(pattern);
-		int rank = calRank(card_to_be_played);
+		int rank = calRank(card_to_be_played,true);
 		int weight = calWeight(pattern);
 		setLastRank(rank);
 		setLastWeight(weight);
-		setLastCards(card_to_be_played);
+		setLastCards(card_to_be_played,true);
 		return true;			
 	}
-	else return false;
+		else 
+		{
+			this.setLastPattern(pattern);
+			int rank = calRank(card_to_be_played,false);
+			int weight = calWeight(pattern);
+			setLastRank(rank);
+			setLastWeight(weight);
+			setLastCards(card_to_be_played,false);
+			return true;	
+		}
+	}
+	else throw new PatternNotRecognizeException(pattern);
+
 	}
 	
 	
@@ -209,8 +288,29 @@ public class RuleController {
 		else return false;
 			
 	}
+	
+	public boolean Straight(ArrayList<Card> arraylist) {
+		int[] array = new int[5];
+		for (int i=0;i<5;i++)
+		{
+			array[i]=arraylist.get(i).getIntFace();
+		}
+		if (array[0]==3&&array[1]==4&&array[2]==5&&array[3]==14&&array[4]==15)return true;
+		else if (array[0]==3&&array[1]==4&&array[2]==5&&array[3]==6&&array[4]==15)return true;
+		else {
+			boolean straight = true;
+			int j = 0;
+			while(straight&&j<4)
+			{
+				if(array[j]!=(array[j+1]-1))straight =false;
+				else j++;
+			}
+			return straight;
+		}
+		
+	}
 			
-	public boolean Straight(ArrayList<Card> arraylist){
+	/*public boolean Straight(ArrayList<Card> arraylist){
 		Collections.sort(arraylist);
 		if(faceToInt(arraylist.get(0).getFace())==10){//not 10JQKA
 		for(int i = 0 ; i < arraylist.size() - 1; i++){
@@ -230,7 +330,7 @@ public class RuleController {
 		return valid;
 				
 			
-	}
+	}*/
 			
 	public boolean Flush(ArrayList<Card> arraylist){
 		boolean flush = true;
@@ -245,7 +345,7 @@ public class RuleController {
 	}
 
 	public boolean StraightFlush(ArrayList<Card> arraylist){
-		return Flush(arraylist)&&Straight(arraylist);
+		return (Flush(arraylist)&&Straight(arraylist));
 		//if (valid) setLastPattern("StraightFlush");
 	}
 	
@@ -348,11 +448,20 @@ public class RuleController {
 		}
 		
 	}
-	public int calRank(ArrayList<Card> arraylist){
+	public int calRank(ArrayList<Card> arraylist,boolean flag){
 		int rank = 0;
+		int[] keeper = new int[arraylist.size()];
 		for(int i = 0 ; i < arraylist.size(); i++){
-		rank += faceToInt(arraylist.get(i).getFace());
+		keeper[i]=arraylist.get(i).getIntFace();
 		}
+		if (flag) {
+			for(int i = 0;i<keeper.length;i++) {
+				if (keeper[i]==14)keeper[i]=1;
+				else if (keeper[i]==15)keeper[i]=2;
+				}
+			for (int i: keeper)rank += i;
+		}
+		else for (int i:keeper)rank += i;
 		return rank;
 	}
 }

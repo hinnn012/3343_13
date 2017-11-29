5ed7f2fc06c8dcd45e79b5c43467a5fc8573005a
sadase <jklifos@gmail.com>
2017-11-29 21:44:12 +0800

    test for main

---
+++ Test/testMain.java
@@ -0,0 +1,15 @@
+package Test;
+import org.junit.Test;
+
+import BigTwo.Main;
+
+public class testMain {
+	
+	@Test
+	public void testMain_1() throws Exception{
+		
+		Main main = new Main();
+		main.main(null);
+		
+	}
+}
